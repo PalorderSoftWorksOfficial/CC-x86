c1c845ba5f6256fd1d8e72d8ff98cd369cbe4a25
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-08-15 23:17:16 +0200

    feat: expand BIOS firmware ABI

--- src/x86/io/bios.lua
+++ src/x86/io/bios.lua
@@ -2,23 +2,39 @@ local bit = bit32
 local U32 = require("src.x86.util.u32")
 
 ---@class BIOS
----Provides the emulator's deliberately small host interrupt interface.
+---Provides the intentionally small firmware ABI exposed through INT 0x80.
 local BIOS = {}
 BIOS.__index = BIOS
+BIOS.VERSION = 0x00010000
 
----Creates a BIOS service layer bound to the CPU.
-function BIOS.new(cpu) return setmetatable({ cpu = cpu }, BIOS) end
+function BIOS.new(cpu)
+    return setmetatable({ cpu = cpu }, BIOS)
+end
 
----Handles a guest software interrupt number.
+---Handles one guest software interrupt. This is not a Linux syscall ABI.
 function BIOS:interrupt(number)
-    if number ~= 0x80 then error(string.format("unsupported interrupt %02X", number)) end
+    if number ~= 0x80 then
+        error(string.format("unsupported interrupt %02X", number))
+    end
+
     local eax = self.cpu.registers:get(0)
     local ebx = self.cpu.registers:get(3)
-    if eax == 1 then print(U32.to_signed(eax))
-    elseif eax == 2 then write(string.char(bit.band(ebx, 0xFF)))
-    elseif eax == 3 then print(self.cpu.memory:read_cstring(ebx))
-    elseif eax == 6 then self.cpu.halted = true
-    else error(string.format("unsupported BIOS call EAX=%08X", eax)) end
+
+    if eax == 0 then
+        self.cpu.registers:set(0, BIOS.VERSION)
+    elseif eax == 1 then
+        print(U32.to_signed(ebx))
+    elseif eax == 2 then
+        write(string.char(bit.band(ebx, 0xFF)))
+    elseif eax == 3 then
+        print(self.cpu.memory:read_cstring(ebx, 4096))
+    elseif eax == 4 then
+        self.cpu.registers:set(0, os.epoch("utc"))
+    elseif eax == 6 then
+        self.cpu.halted = true
+    else
+        error(string.format("unsupported BIOS call EAX=%08X", eax))
+    end
 end
 
 return BIOS
